3f5e82d49206c610819b7ddb2c5b1270ae9d0dcb
Roman-code-web <selenerg.275@gmail.com>
2023-01-03 20:00:58 -0500

    logica terminada

--- src/app/componentes/componentes.module.ts
+++ src/app/componentes/componentes.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from './form/form.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { FormComponent } from './form/form.component';
     FormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports:[
     FormComponent
--- src/app/componentes/form/form.component.ts
+++ src/app/componentes/form/form.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
+  num1!:number;
+  num2!:number;
+  resultado:string="no eligio una opción";
+  operacion!:string;
+  verificacion!:boolean;
+  mensaje!:string;
+  calcular(oper:string){
+    //validacion
+    if(this.num1==0 || this.num1==null){
+      this.verificacion=false;
+      this.mensaje="Ingresar numero 1";
+      return
+    }else if(this.num2==0 || this.num2==null){
+      this.verificacion=false;
+      this.mensaje="Ingresar numero 2";
+      return
+    }else{
+      this.verificacion=true;
+    }
+    //calcular
+    if(oper=="+"){
+      this.resultado = String(this.num1 + this.num2) ;
+      this.operacion="+"
+    }else if(oper=="-"){
+      this.resultado=String(this.num1 - this.num2);
+      this.operacion="-"
+    }else if(oper=="*"){
+      this.resultado=String(this.num1 * this.num2);
+      this.operacion="*"
+    }else if (oper=="/"){
+      this.resultado=String(this.num1 / this.num2);
+      this.operacion="/"
+    }else if(oper==""){
+      this.resultado="no eligio una opcion";
+      this.operacion="";
+    }
+  }
+
 
 }
